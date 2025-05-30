93ec450edbd29b0ec053a325c84194211e162d64
RoopChoudhary01 <RoopChoudhary01>
2025-05-30 14:47:15 +0530

    remove some field from coupon

--- resources/views/coupons/partials/_form.blade.php
+++ resources/views/coupons/partials/_form.blade.php
@@ -24,23 +24,6 @@
     </div>
 
     <div class="mb-3 col-md-6">
-        <label for="coupon_status" class="form-label">{{ __('Coupon Status') }}</label>
-        <select name="coupon_status" class="form-control @error('coupon_status') is-invalid @enderror" id="basicSelect">
-            <option value="">Select</option>
-            <option value="used" {{ old('coupon_status', $coupon->coupon_status ?? '') == 'used' ? 'selected' : '' }}>{{ __('Used') }}</option>
-            <option value="notused" {{ old('coupon_status', $coupon->coupon_status ?? '') == 'notused' ? 'selected' : '' }}>{{ __('Not Used') }}</option>
-            <option value="cancelled" {{ old('coupon_status', $coupon->coupon_status ?? '') == 'cancelled' ? 'selected' : '' }}>{{ __('Cancelled') }}</option>
-        </select>
-        @error('coupon_status') <div class="invalid-feedback">{{ $message }}</div> @enderror
-    </div>
-
-    <div class="mb-3 col-md-6">
-        <label for="used_by" class="form-label">{{ __('Used By') }}</label>
-        <input type="number" name="used_by" placeholder="{{ __('Enter user mobile') }}" class="form-control @error('used_by') is-invalid @enderror" value="{{ old('used_by', $coupon->used_by ?? '') }}">
-        @error('used_by') <div class="invalid-feedback">{{ $message }}</div> @enderror
-    </div>
-
-    <div class="mb-3 col-md-6">
         <label for="status_date" class="form-label">{{ __('Status Date') }}</label>
         <input type="date" name="status_date" class="form-control @error('status_date') is-invalid @enderror" value="{{ old('status_date', $coupon->status_date ?? '') }}">
         @error('status_date') <div class="invalid-feedback">{{ $message }}</div> @enderror
