47dbf288e44b3445bdc9b4fac391068d006a7aaa
RoopChoudhary01 <RoopChoudhary01>
2025-06-02 15:11:37 +0530

    payout issue

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\Admin\Auth\LoginController;
 use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\Admin\UserController;
 use App\Http\Controllers\Admin\CouponController;
+use App\Http\Controllers\Admin\PayoutController;
 
 Route::middleware(['guest'])->group(function () {
     Route::get('/', [LoginController::class, 'showLoginForm'])->name('login');
@@ -21,6 +22,8 @@ Route::prefix('admin')->group(function () {
         Route::post('/coupons/import', [CouponController::class, 'import'])->name('coupons.import');
         Route::post('coupons/update/stauts', [CouponController::class, 'updateStatus'])->name('coupons.updateStatus');
         Route::resource('coupons', CouponController::class);
+
+        Route::get('payouts', [PayoutController::class, 'index'])->name('payouts');
         
 
         Route::get('logout', [LoginController::class, 'logout'])->name('logout');
