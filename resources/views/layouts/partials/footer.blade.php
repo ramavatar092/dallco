ab4457a2a70f3964b9a82fa83265845df6ef4756
Ramavatar Choudhary <ramavatar092@gmail.com>
2025-06-04 09:14:58 +0000

    server side footer some changes

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -1,6 +1,6 @@
 <!-- BEGIN: Footer-->
 <footer class="footer footer-static footer-light">
-    <p class="clearfix mb-0"><span class="float-left d-inline-block">2020 &copy; PIXINVENT</span><span class="float-right d-sm-inline-block d-none">Crafted with<i class="bx bxs-heart pink mx-50 font-small-3"></i>by<a class="text-uppercase" href="#" target="_blank">Pixinvent</a></span>
+    <p class="clearfix mb-0"><span class="float-left d-inline-block">© Copyright {{ config('app.name') }}</span><span class="float-right d-sm-inline-block d-none">All Rights Reserved<i class="bx bxs-heart pink mx-50 font-small-3"></i>Designed by<a class="text-uppercase" href="https://softartithub.com" target="_blank">Softart It Hub</a></span>
         <button class="btn btn-primary btn-icon scroll-top" type="button"><i class="bx bx-up-arrow-alt"></i></button>
     </p>
 </footer>
