6661d81b544fd36d92b5f5d6259bfef17efdee9a
ramavatar092 <73327469+ramavatar092@users.noreply.github.com>
2025-06-04 14:37:01 +0530

    dashboard code

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -4,11 +4,30 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\User;
+use App\Models\ScanLog;
+use App\Models\Payout;
+use Carbon\Carbon;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        return view('dashboard');
+        $totalUsers = User::count();
+        $totalScans = ScanLog::count();
+        $totalScanAmount = ScanLog::sum('scan_amount');
+        $totalPayout = Payout::sum('amount');
+
+        $last30Days = Carbon::now()->subDays(30);
+
+        $newUsers = User::where('created_at', '>=', $last30Days)->count();
+        $scansLast30 = ScanLog::where('created_at', '>=', $last30Days)->count();
+        $amountLast30 = ScanLog::where('created_at', '>=', $last30Days)->sum('scan_amount');
+        $payoutLast30 = Payout::where('created_at', '>=', $last30Days)->sum('amount');
+
+        return view('dashboard', compact(
+            'totalUsers', 'totalScans', 'totalScanAmount', 'totalPayout',
+            'newUsers', 'scansLast30', 'amountLast30', 'payoutLast30'
+        ));
     }
 }
--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -2,9 +2,9 @@
 <div class="main-menu menu-fixed menu-light menu-accordion menu-shadow" data-scroll-to-active="true">
     <div class="navbar-header">
         <ul class="nav navbar-nav flex-row">
-            <li class="nav-item mr-auto"><a class="navbar-brand" href="../../../html/ltr/vertical-menu-boxicons-template/index.html">
+            <li class="nav-item mr-auto"><a class="navbar-brand" href="{{ route('dashboard') }}">
                     <div class="brand-logo"><img class="logo" src="../../../app-assets/images/logo/logo.png" /></div>
-                    <h2 class="brand-text mb-0">Frest</h2>
+                    <h2 class="brand-text mb-0">{{ config('app.name') }}</h2>
                 </a></li>
             <li class="nav-item nav-toggle"><a class="nav-link modern-nav-toggle pr-0" data-toggle="collapse"><i class="bx bx-x d-block d-xl-none font-medium-4 primary toggle-icon"></i><i class="toggle-icon bx bx-disc font-medium-4 d-none d-xl-block collapse-toggle-icon primary" data-ticon="bx-disc"></i></a></li>
         </ul>
