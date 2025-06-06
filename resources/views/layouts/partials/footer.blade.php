20ded362d55604bd9e76e01838db19f1b625031c
RoopChoudhary01 <RoopChoudhary01>
2025-06-06 19:19:26 +0530

    footer

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -1,6 +1,6 @@
 <!-- BEGIN: Footer-->
 <footer class="footer footer-static footer-light">
-    <p class="clearfix mb-0"><span class="float-left d-inline-block">© Copyright {{ config('app.name') }}</span><span class="float-right d-sm-inline-block d-none">All Rights Reserved<i class="bx bxs-heart pink mx-50 font-small-3"></i>Designed by<a class="text-uppercase" href="https://softartithub.com" target="_blank">Softart It Hub</a></span>
+    <p class="clearfix mb-0"><span class="float-left d-inline-block">© Copyright {{ config('app.name') }}</span><span class="float-right d-sm-inline-block d-none">All Rights Reserved<i class="bx bxs-heart pink mx-50 font-small-3"></i>Designed by<a class="text-uppercase" href="https://softartithub.com" target="_blank">OBO DIGITAL</a></span>
         <button class="btn btn-primary btn-icon scroll-top" type="button"><i class="bx bx-up-arrow-alt"></i></button>
     </p>
 </footer>
